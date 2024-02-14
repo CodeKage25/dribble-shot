983d7c9b6bdf158a19790f6e01878e8714a66726
Babatunde Abdulkareem <babzchizzy27@gmail.com>
2024-02-14 22:44:05 +0100

    changes

--- src/app/components/Nav.tsx
+++ src/app/components/Nav.tsx
@@ -20,17 +20,18 @@ const Nav = () => {
     ]
 
     return (
-        <div className="flex justify-around">
-            <div>
+        <div className="flex justify-around w-[100%]">
+            <div className="w-[12%]">
                 Wumbo
             </div>
             {navList.map((list, id) => (
-                <div key={id} className= "flex justify-between">
+                <div key={id} className= "flex justify-between w-[64%]">
                     {list.name}
                 </div>
             ))}
-            <div>
-                <Button className="h-[43px]" variant="default" size="default">Download</Button>
+            <div className="w-[25%]">
+                <Button className="h-[43px] bg-white text-[
+#3B82F6]" variant="default" size="default">Download</Button>
             </div>
         </div>
     )
